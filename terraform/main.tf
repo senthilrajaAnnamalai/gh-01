6eeca1b85ac32923b5c9c7efdae6ff5bf9d45ec2
senthilrajaAnnamalai <asenthil29jan@gmail.com>
2024-04-03 22:48:52 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,3 +6,12 @@ terraform {
           }
         }
   }
+
+resource "aws_instance" "demo" {
+ ami = “ami-00831fc7c1e3ddc60”
+ instance_type              = “t2.micro”
+
+ tags = {
+   name= "Demo System"
+ }
+}
