853ff7698983403a698173f6e45fd93bc0a69dae
senthilrajaAnnamalai <asenthil29jan@gmail.com>
2024-04-03 22:51:02 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,8 +8,8 @@ terraform {
   }
 
 resource "aws_instance" "demo" {
- ami = “ami-00831fc7c1e3ddc60”
- instance_type              = “t2.micro”
+ ami = "ami-00831fc7c1e3ddc60"
+ instance_type              = "t2.micro"
 
  tags = {
    name= "Demo System"
